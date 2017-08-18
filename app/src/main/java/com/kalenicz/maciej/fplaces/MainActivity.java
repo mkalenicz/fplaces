595d5cb983e2c583365bce1af02b4021ca9d1129
maciej <maciej.kalenicz@gmail.com>
2017-08-18 15:14:20 +0200

    Toast (Your current location) onOptionsItemSelected

--- app/src/main/java/com/kalenicz/maciej/fplaces/MainActivity.java
+++ app/src/main/java/com/kalenicz/maciej/fplaces/MainActivity.java
@@ -3,7 +3,8 @@ package com.kalenicz.maciej.fplaces;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
-
+import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -20,5 +21,17 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.options_menu, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.options_info:
+
+                //showInfoDialog();
+               Toast.makeText(this, "Your current location is: ", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
