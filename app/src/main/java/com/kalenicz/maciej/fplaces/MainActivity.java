95bb893df3700ce3f314165c9f6617d8c1227e4f
maciej <maciej.kalenicz@gmail.com>
2017-08-18 20:27:08 +0200

    Show current location using LastKnownLocation - faster localization info and better battery life

--- app/src/main/java/com/kalenicz/maciej/fplaces/MainActivity.java
+++ app/src/main/java/com/kalenicz/maciej/fplaces/MainActivity.java
@@ -1,19 +1,83 @@
 package com.kalenicz.maciej.fplaces;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private FusedLocationProviderClient mFusedLocationClient;
+    public TextView mLatitudeText;
+    public TextView mLongitudeText;
+    public TextView mAccuracy;
+    public TextView mAltitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mLatitudeText = (TextView) findViewById((R.id.latTextView));
+        mLongitudeText = (TextView) findViewById((R.id.lonTextView));
+        mAccuracy = (TextView) findViewById((R.id.accTextView));
+        mAltitude = (TextView) findViewById((R.id.altTextView));
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }
+        }
+
+    }
+
+
+    private void getLastLocation() {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            return;
+        }
+        mFusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if (location != null) {
+                            mLatitudeText.setText(String.format("Latitude: %1$,.5f", location.getLatitude()));
+                            mLongitudeText.setText(String.format("Longitude: %1$,.5f",location.getLongitude()));
+                            mAccuracy.setText(String.format("Accuracy: %1$,.5f ", location.getAccuracy()));
+                            mAltitude.setText(String.format("Altitude: %1$,.5f ", location.getAltitude()));
+                        }
+
+                    }
+                });
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults.length <= 0 || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+            finish();
+            Toast.makeText(MainActivity.this, "Permission denied", Toast.LENGTH_SHORT).show();
+        }
     }
 
 
@@ -27,11 +91,11 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.options_info:
-
-                //showInfoDialog();
-               Toast.makeText(this, "Your current location is: ", Toast.LENGTH_SHORT).show();
+                getLastLocation();
                 break;
         }
         return super.onOptionsItemSelected(item);
     }
+
+
 }
